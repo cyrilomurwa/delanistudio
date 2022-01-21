5ba2039101faa0b7f8a308061a424ff4b8a294ce
Cyril Ondieki Omurwa <COMURWA@safaricom.co.ke>
2022-01-21 14:23:37 +0300

    update index.html complete contact section form design

--- js/script.js
+++ js/script.js
@@ -30,5 +30,71 @@ $(document).ready(function(){
     }).mouseleave(function () {
       $('#work1-popup').hide("slow");
     });
+    $('#work2').mouseenter(function () {
+      $('.work-heading').filter('#work2-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work2-popup').hide("slow");
+    });
+    $('#work3').mouseenter(function () {
+      $('.work-heading').filter('#work3-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work3-popup').hide("slow");
+    });
+    $('#work4').mouseenter(function () {
+      $('.work-heading').filter('#work4-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work4-popup').hide("slow");
+    });
+    $('#work5').mouseenter(function () {
+      $('.work-heading').filter('#work5-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work5-popup').hide("slow");
+    });
+    $('#work6').mouseenter(function () {
+      $('.work-heading').filter('#work6-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work6-popup').hide("slow");
+    });
+    $('#work7').mouseenter(function () {
+      $('.work-heading').filter('#work7-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work7-popup').hide("slow");
+    });
+    $('#work8').mouseenter(function () {
+      $('.work-heading').filter('#work8-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work8-popup').hide("slow");
+    });
+});
 
+//retrieve form details
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+
+      var pnameInput = $("input#pname").val();
+      var emailInput = $("input#email").val();
+      var country = $("#country").val();
+      var colour = $("input#colour").val();
+      var dob = $("input#dob").val();
+      var gender = $("input:radio[name=gender]:checked").val();
+
+       var music = [];
+           $("input:checkbox[name='music']:checked").each(function(){    
+               cricketer.push($(this).val());    		
+           });
+           alert("The selected chocolate are: " + cricketer.join(", "));
+      
+      $(".pname").text(pnameInput);
+      $(".email").text(emailInput);
+      $(".gender").text(gender);
+      $(".country").text(country);
+      $(".colour").text(colour);
+      $(".dob").text(dob);
+  
+  
+  
+      alert("Thank you for your Message. We have received your information as below"+"\\n"+"Your Name "+pnameInput+" Gender "+gender+" Date of Birth "+dob+" Email "+emailInput+" "+"\\n"+"Country "+country+" Favourite Color "+colour+" Music Genre "+ cricketer.join(", ")+"");
+
+      event.preventDefault();
+  });
 });
