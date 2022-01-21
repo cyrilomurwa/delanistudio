e6662fc903a12a417893bc356bdbb122e0de0460
Cyril Ondieki Omurwa <COMURWA@safaricom.co.ke>
2022-01-21 07:42:21 +0300

    update index.html and javascript function for profile section

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
-
+//functions to hide and display items on what to do section
     $(".design").click(function() {
       $("#design, .design").toggle("slow");
-    })
+    });
 
     $("#design").click(function() {
       $(".design, #design").toggle("slow");
-    })
+    });
 
     $(".development").click(function() {
       $("#development, .development").toggle("slow");
-    })
+    });
 
     $("#development").click(function() {
       $(".development, #development").toggle("slow");
-    })
+    });
 
     $(".productManagement").click(function() {
       $("#productManagement, .productManagement").toggle("slow");
-    })
+    });
 
     $("#productManagement").click(function() {
       $(".productManagement, #productManagement").toggle("slow");
-    })
+    });
 
-})
+ //Portal hover function
+    $('#work1').mouseenter(function () {
+      $('.work-heading').filter('#work1-popup').show("slow");
+    }).mouseleave(function () {
+      $('#work1-popup').hide("slow");
+    });
+
+});
